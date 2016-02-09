5a6070ef1a1e58843f90373771702448c6f8ade6
Alan Hamlett <alan.hamlett@gmail.com>
2016-02-08 17:36:42 -0800

    add missing fields to package.json

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-c9-wakatime===========Metrics, insights, and time tracking automatically generated from your programming activity.Screen Shots------------![Project Overview](https://wakatime.com/static/img/ScreenShots/ScreenShot-2014-10-29.png)Troubleshooting---------------For more general troubleshooting information, see [wakatime/wakatime#troubleshooting](https://github.com/wakatime/wakatime#troubleshooting).
+c9-wakatime
+===========
+
+Metrics, insights, and time tracking automatically generated from your programming activity.
+
+
+Screen Shots
+------------
+
+![Project Overview](https://wakatime.com/static/img/ScreenShots/ScreenShot-2014-10-29.png)
+
+
+Troubleshooting
+---------------
+
+For general troubleshooting information, see [wakatime/wakatime#troubleshooting](https://github.com/wakatime/wakatime#troubleshooting).
--- tests.js
+++ tests.js
@@ -2,14 +2,14 @@
 "use mocha";
 
 define(function(require, exports, module) {
-    main.consumes = ["plugin.test", "myplugin"];
+    main.consumes = ["plugin.test", "wakatime"];
     main.provides = [];
     return main;
 
     function main(options, imports, register) {
         var test = imports["plugin.test"];
-        var myplugin = imports.myplugin;
-        
+        var wakatime = imports.wakatime;
+
         var describe = test.describe;
         var it = test.it;
         var before = test.before;
@@ -18,30 +18,30 @@ define(function(require, exports, module) {
         var afterEach = test.afterEach;
         var assert = test.assert;
         var expect = test.expect;
-        
+
         /***** Initialization *****/
-        
-        describe(myplugin.name, function(){
+
+        describe(wakatime.name, function(){
             this.timeout(2000);
-            
+
             beforeEach(function() {
             });
-        
+
             afterEach(function () {
             });
-            
+
             it("has a sync test", function() {
             });
-            
+
             it("has a async test", function(done) {
                 done();
             });
-            
+
             it("has a failing test", function() {
                 assert.equal(10, 11);
             });
         });
-        
+
         register(null, {});
     }
 });
