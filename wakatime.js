7ee483959dfccde7740033306a88d72d26a210c5
Alan Hamlett <alan.hamlett@gmail.com>
2016-02-20 16:15:31 -0800

    v1.0.6

--- wakatime.js
+++ wakatime.js
@@ -33,7 +33,7 @@ define(function(require, exports, module) {
         var cachedApiKey = null;
 
         function init() {
-            pluginVersion = options.version || '1.0.5';
+            pluginVersion = options.version || '1.0.6';
             c9Version = c9.version.split(' ')[0];
             if (settings.get("user/wakatime/@debug"))
                 console.log("Initializing WakaTime v" + pluginVersion);
